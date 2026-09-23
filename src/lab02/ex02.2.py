4263a69712bc82ccf04ead6e462c35f537675ca3
eliqqww <elizavetfrixx@gmail.com>
2026-09-23 16:04:55 +0300

    labs

---
+++ src/lab02/ex02.2.py
@@ -0,0 +1,12 @@
+def row_sums(mat):
+    res=[]
+    for i in mat:
+        if len(i)!=len(mat[0]):
+            raise ValueError 
+    for i in range(len(mat)):
+        res.append(sum(mat[i]))
+    return res
+print(row_sums([[1, 2, 3], [4, 5, 6]]))
+print(row_sums([[-1, 1], [10, -10]]))
+print(row_sums([[0, 0], [0, 0]]))
+# print(row_sums([[1, 2], [3]]))
