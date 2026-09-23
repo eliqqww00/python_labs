dde64704e69cfb660101455712d9786dc30023b1
eliqqww <elizavetfrixx@gmail.com>
2026-09-23 09:45:17 +0300

    labs

--- src/lab02/ex01.3.py
+++ src/lab02/ex01.3.py
@@ -6,3 +6,6 @@ def flatten(mat):
         vector.extend(i)
     return vector
 print(flatten([[1, 2], [3, 4]]))
+print(flatten([[1, 2], (3, 4, 5)]))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
