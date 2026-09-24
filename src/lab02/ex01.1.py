e850c5aa6c1ec58637f684ce738e50e43fabb4b8
eliqqww <elizavetfrixx@gmail.com>
2026-09-24 20:04:09 +0300

    labs

--- src/lab02/ex01.1.py
+++ src/lab02/ex01.1.py
@@ -1,6 +1,17 @@
-def min_max(nums):
+def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+    ''' Вычисляет максимум и минимум списка
+
+    Args: 
+        nums: Список чисел (целых и вещественных)
+
+    Returns:
+        Кортеж (минимум, максимум)
+
+    Raises:
+        ValueError: Если список пустой
+    '''
     if len(nums)==0: 
-        raise ValueError
+        raise ValueError ('пустой список')
     minimum=nums[0]
     maximim=nums[0]
     for i in nums:
@@ -10,8 +21,13 @@ def min_max(nums):
             maximim=i
     return (minimum,maximim)
 
-print(min_max([3, -1, 5, 5, 0]))
-print(min_max([42]))
-print(min_max([-5, -2, -9]))
-print(min_max([1.5, 2, 2.0, -3.1]))
-print(min_max([]))
+print(f'''
+тест кейсы:
+
+[3, -1, 5, 5, 0] -> {min_max([3, -1, 5, 5, 0])}
+[42] -> {min_max([42])}
+[-5, -2, -9] -> {min_max([-5, -2, -9])}
+[1.5, 2, 2.0, -3.1] -> {min_max([1.5, 2, 2.0, -3.1])}
+''')
+# Тест кейс который выводит ошибку ValueError
+# print(f'[] -> {min_max([])}')
